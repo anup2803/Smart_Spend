5c5fce750f464a33fca4f7a35c39eaa11e760b5f
Anup Kumal <anupbinu2803@gmail.com>
2025-09-17 10:41:33 +0545

    Database connection (Render will pull from environment variable)

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/smart_spend'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/smart_spend'  its used for development 
+
+    #but for production Database connection (Render will pull from environment variable)
+    SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/smart_spend'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'super_secret_key123'
 
