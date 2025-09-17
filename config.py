902a13504b66c2260478d2e0295099d7be18e889
Anup Kumal <anupbinu2803@gmail.com>
2025-09-17 21:07:53 +0545

    change in config for productions

--- config.py
+++ config.py
@@ -1,33 +1,24 @@
 import os
 
 class Config:
-   
-    # Database configuration
-    # Use DATABASE_URL from environment (Render/production) or fallback to local MySQL
+    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         "mysql+pymysql://root:@localhost/smart_spend"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
     # Security
-   
     SECRET_KEY = os.environ.get('SECRET_KEY', 'super_secret_key123')
 
-
-    
-    # Flask-Mail configuration
-    
+    # Mail
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'smartspend94@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'jgpt pkuf xrxb ykpt')  # App password
-    MAIL_DEFAULT_SENDER = ('SmartSpend', 'smartspend94@gmail.com')
-   
-    # Flask app URL settings
- # ⚠️ Only use SERVER_NAME in production, not during local development
-    if os.environ.get("FLASK_ENV") == "production":
-        SERVER_NAME = 'smart-spend-1.onrender.com'
-        PREFERRED_URL_SCHEME = 'https'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'jgpt pkuf xrxb ykpt')
+    MAIL_DEFAULT_SENDER = 'SmartSpend <smartspend94@gmail.com>'
+
+
+    SERVER_NAME = 'smart-spend-1.onrender.com'
+    PREFERRED_URL_SCHEME = 'https'
